ed2b4b8dcaab6265de6a0ff90d85e5907a4cf348
Serge Kovbasiuk <isserge@protonmail.com>
2025-08-03 16:36:27 +0700

    feat: Add BlockBuilder for creating test Block instances

--- src/test_helpers/mod.rs
+++ src/test_helpers/mod.rs
@@ -3,7 +3,9 @@
 mod log;
 mod receipt;
 mod transaction;
+mod block;
 
 pub use log::LogBuilder;
 pub use receipt::ReceiptBuilder;
 pub use transaction::TransactionBuilder;
+pub use block::BlockBuilder;
