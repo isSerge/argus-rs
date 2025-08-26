1167fd5bc772c3f6f9cb4b8b69ba8ba18cec2866
Serge Kovbasiuk <isserge@protonmail.com>
2025-08-26 09:04:03 +0700

    feat: add MonitorMatch constructor methods

--- src/models/monitor_match.rs
+++ src/models/monitor_match.rs
@@ -16,46 +16,92 @@ pub struct MonitorMatch {
     /// Name of the notifier that will handle notifications for this match.
     pub notifier_name: String,
 
-    /// The data associated with the match, which can vary based on the type (e.g., transaction details, log details).
+    /// The data associated with the match, which can vary based on the type
+    /// (e.g., transaction details, log details).
     pub match_data: MatchData,
 }
 
 impl MonitorMatch {
     /// Create MonitorMatch instance from transaction match data
-    pub fn new_tx_match() -> Self {
-        unimplemented!()
+    pub fn new_tx_match(
+        monitor_id: i64,
+        monitor_name: String,
+        notifier_name: String,
+        block_number: u64,
+        transaction_hash: TxHash,
+        details: serde_json::Value,
+    ) -> Self {
+        Self {
+            monitor_id,
+            monitor_name,
+            notifier_name,
+            match_data: MatchData::Transaction(TransactionDetails {
+                block_number,
+                transaction_hash,
+                details,
+            }),
+        }
     }
 
     /// Create MonitorMatch instance from log match data
-    pub fn new_log_match() -> Self {
-        unimplemented!()
+    pub fn new_log_match(
+        monitor_id: i64,
+        monitor_name: String,
+        notifier_name: String,
+        block_number: u64,
+        transaction_hash: TxHash,
+        contract_address: Address,
+        log_index: u64,
+        log_name: String,
+        log_params: serde_json::Value,
+    ) -> Self {
+        Self {
+            monitor_id,
+            monitor_name,
+            notifier_name,
+            match_data: MatchData::Log(LogDetails {
+                block_number,
+                transaction_hash,
+                contract_address,
+                log_index,
+                details: LogMatch {
+                    name: log_name,
+                    params: log_params,
+                },
+            }),
+        }
     }
 }
 
+/// Represents the data associated with a monitor match.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(tag = "type")]
-pub enum MatchData{
+pub enum MatchData {
+    /// A transaction match.
     Transaction(TransactionDetails),
+    /// A log/event match.
     Log(LogDetails),
 }
 
+/// Details about a transaction match.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct TransactionDetails {
     /// The block number where the match was found.
     pub block_number: u64,
-    
+
     /// The transaction hash associated with the match.
     pub transaction_hash: TxHash,
 
     /// Additional details about the transaction.
-    pub details: serde_json::Value
+    pub details: serde_json::Value,
 }
 
+/// Details about a log/event match.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct LogDetails {
     /// The block number where the match was found.
     pub block_number: u64,
-    
+
     /// The transaction hash associated with the match.
     pub transaction_hash: TxHash,
 
@@ -63,12 +109,13 @@ pub struct LogDetails {
     pub contract_address: Address,
 
     /// The log index within the block, if the trigger was an event.
-    pub log_index: Option<u64>,
+    pub log_index: u64,
 
     /// Additional details about the log/event.
     pub details: LogMatch,
 }
 
+/// Details about a log/event match.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct LogMatch {
     /// The name of the log/event.
