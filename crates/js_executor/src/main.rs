ebb35386226cf09b0d1809414427374219be78d5
Serge Kovbasiuk <isserge@protonmail.com>
2025-09-23 16:56:38 +0700

    feat: add tower-http dependency and implement HTTP tracing in js_executor

--- crates/js_executor/src/main.rs
+++ crates/js_executor/src/main.rs
@@ -1,44 +1,55 @@
 mod engine;
 
 use argus_models::monitor_match::MonitorMatch;
-use axum::{Json, Router, http::StatusCode, routing::post};
+use axum::{Json, Router, routing::post};
 use serde::{Deserialize, Serialize};
+use tower_http::trace::TraceLayer;
+
+use crate::engine::{JsRunnerError, execute_script};
+
+// TODO: move to argus_models
+#[derive(Deserialize)]
+struct ExecutionRequest {
+    script: String,
+    context: MonitorMatch,
+}
+
+// TODO: move to argus_models
+#[derive(Serialize)]
+struct ExecutionResponse {
+    result: MonitorMatch,
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    error: String,
+}
+
+impl From<JsRunnerError> for ErrorResponse {
+    fn from(err: JsRunnerError) -> Self {
+        ErrorResponse { error: err.to_string() }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/execute", post(execute_action_handler));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let app =
+        Router::new().route("/execute", post(execute_handler)).layer(TraceLayer::new_for_http());
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+    tracing::info!("Listening on {}", addr.port());
 
     axum::serve(listener, app).await.unwrap();
 }
 
-#[derive(Deserialize)]
-struct ExecuteActionRequest {
-    script: String,
-    context: MonitorMatch,
-}
-
-#[derive(Serialize)]
-struct ExecuteActionResponse {
-    modified_match: MonitorMatch,
-}
-
-async fn execute_action_handler(
-    Json(payload): Json<ExecuteActionRequest>,
-) -> (StatusCode, Json<ExecuteActionResponse>) {
-    let context = payload.context;
-    let script = payload.script;
-
-    match engine::execute_script(script, &context).await {
-        Ok(modified_match) => (StatusCode::OK, Json(ExecuteActionResponse { modified_match })),
-        Err(err) => {
-            tracing::error!("Error executing action: {}", err);
-            (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(ExecuteActionResponse { modified_match: context }),
-            )
-        }
+async fn execute_handler(
+    Json(payload): Json<ExecutionRequest>,
+) -> Result<Json<ExecutionResponse>, Json<ErrorResponse>> {
+    match execute_script(payload.script, &payload.context).await {
+        Ok(result) => Ok(Json(ExecutionResponse { result })),
+        Err(e) => Err(Json(e.into())),
     }
 }
