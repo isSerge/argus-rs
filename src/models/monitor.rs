08d4c869d217afcfd5a9e25758651b6fb1c4c726
Serge Kovbasiuk <isserge@protonmail.com>
2025-10-30 13:55:11 +0700

    fix: formatting

--- src/models/monitor.rs
+++ src/models/monitor.rs
@@ -22,7 +22,8 @@ pub struct MonitorConfig {
     pub address: Option<String>,
 
     /// The name of the ABI for the contract being monitored (e.g., "erc20").
-    /// This name references an ABI stored in the database. If `None`, the monitor will not decode logs.
+    /// This name references an ABI stored in the database. If `None`, the
+    /// monitor will not decode logs.
     #[serde(default, alias = "abi")]
     pub abi_name: Option<String>,
 
