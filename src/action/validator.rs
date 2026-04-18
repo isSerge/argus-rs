eb0b64c89b494187f375664f1f2711e1a0b969a8
Serge Kovbasiuk <isserge@protonmail.com>
2026-04-18 10:36:07 +0700

    fix: clippy

--- src/action/validator.rs
+++ src/action/validator.rs
@@ -69,7 +69,7 @@ impl ActionValidator {
         let action_id = action.id.as_ref().ok_or(PersistenceError::InvalidInput(
             "Action ID is required for update".to_string(),
         ))?;
-        if self.repo.get_action_by_id(&self.network_id, &action_id).await?.is_none() {
+        if self.repo.get_action_by_id(&self.network_id, action_id).await?.is_none() {
             return Err(ActionValidationError::Persistence(PersistenceError::NotFound));
         }
 
